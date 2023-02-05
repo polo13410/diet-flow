e6f121b2b64d8b097064228e0be9cc0d269fa3a8
Polo13410 <polo13410@hotmail.fr>
2023-02-05 11:19:39 +0100

    upd: vite starter
    & pkg updates

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,34 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
 
-import App from './App.vue'
-import router from './router'
+import App from "./App.vue";
+import router from "./router";
 
-import './assets/main.css'
+import "./assets/main.css";
 
-const app = createApp(App)
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-app.use(createPinia())
-app.use(router)
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCBlJVnmqA6-cmXhxAolP3i67xtkNblHT0",
+  authDomain: "dietflow-13664.firebaseapp.com",
+  projectId: "dietflow-13664",
+  storageBucket: "dietflow-13664.appspot.com",
+  messagingSenderId: "882720445844",
+  appId: "1:882720445844:web:eb97dacda26ec939582b81",
+  measurementId: "G-T9BQBZE4PX"
+};
 
-app.mount('#app')
+// Initialize Firebase
+
+const app = createApp(initializeApp(firebaseConfig));
+
+app.use(createPinia());
+app.use(router);
+
+app.mount("#app");
